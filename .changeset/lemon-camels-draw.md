04a05fb490433f44ac2b069d6f998e7c11c2e685
samuel-slavik <samuel.slavik@firma.seznam.cz>
2026-04-23 17:42:31 +0200

    feat(): add package hooks

---
+++ .changeset/lemon-camels-draw.md
@@ -0,0 +1,9 @@
+---
+"@jakubcata/hooks": major
+---
+
+Initial release of the package `hooks`.
+
+- **What** Initial release of the package `hooks`.
+- **Why** Nothing.
+- **How** Nothing.
---
+++ packages/hooks/src/hooks/useBem/useBem.ts
@@ -0,0 +1,80 @@
+export function useBem(base: string) {
+    const bem = (
+        name?: string | Record<string, boolean>,
+        ...names: Array<string | Record<string, boolean>>
+    ) => {
+        const classnames: string[] = [];
+
+        const addClass = (classname: string) => {
+            if (classname && !classnames.includes(classname)) {
+                classnames.push(classname);
+            }
+        };
+
+        const handleObject = (
+            obj: Record<string, boolean>,
+            prefixBase = true
+        ) => {
+            for (const [key, value] of Object.entries(obj)) {
+                if (value) {
+                    if (key === base) {
+                        addClass(base);
+                    } else {
+                        addClass(prefixBase ? `${base}__${key}` : key);
+                    }
+                }
+            }
+        };
+
+        if (name === base || name === undefined) {
+            addClass(base);
+        } else {
+            if (typeof name === 'string' && name !== '') {
+                addClass(`${base}__${name}`);
+            }
+
+            if (typeof name === 'object') {
+                handleObject(name);
+            }
+        }
+
+        for (const classname of names) {
+            if (classname === base) {
+                addClass(base);
+            } else {
+                if (typeof classname === 'string' && classname !== '') {
+                    addClass(classname);
+                }
+
+                if (typeof classname === 'object') {
+                    handleObject(classname, false);
+                }
+
+                addClass(base);
+            }
+        }
+
+        return classnames.join(' ');
+    };
+
+    const modifier = (modifier: string | Record<string, boolean>) => {
+        if (typeof modifier === 'string' && modifier !== '') {
+            return `${base}--${modifier}`;
+        }
+
+        if (typeof modifier === 'object') {
+            return Object.entries(modifier)
+                .filter(([, value]) => value)
+                .map(([key]) => `${base}--${key}`)
+                .join(' ');
+        }
+
+        return '';
+    };
+
+    return {
+        bem,
+        modifier,
+        base,
+    };
+}
